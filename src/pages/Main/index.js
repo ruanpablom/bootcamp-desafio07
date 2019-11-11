c9d673fb506bc80cca5c2adefa890d602de0ccb6
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2019-11-11 16:29:28 -0300

    remove propTypes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { FlatList } from 'react-native';
 
